aff9c8023c20f68d51e345c2b6df3ee8da9b972f
Sugar <145131647+Sugarph@users.noreply.github.com>
2025-02-16 02:43:55 +0700

    Buzzer test

--- ReceiverBoard.ino
+++ ReceiverBoard.ino
@@ -1,6 +1,5 @@
 int segmentPins[7] = {2, 3, 4, 5, 6, 7, 8};
-
-
+int buzzerPin = 9; 
 int digits[10][7] = {
   {1,1,1,1,1,1,0}, // 0
   {0,1,1,0,0,0,0}, // 1
@@ -13,17 +12,28 @@ int digits[10][7] = {
   {1,1,1,1,1,1,1}, // 8
   {1,1,1,1,0,1,1}  // 9
 };
+
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
+  
   for (int i = 0; i < 7; i++) {
     pinMode(segmentPins[i], OUTPUT);
   }
+
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    int receivedNumber = Serial.parseInt(); 
-    displayDigit(receivedNumber);
+    int receivedNumber = Serial.parseInt();
+    
+    if (receivedNumber == 100) {  // Signal for start
+      startBeep();
+    } else if (receivedNumber == 200) {  // Signal for end
+      endBeep();
+    } else {
+      displayDigit(receivedNumber);
+    }
   }
 }
 
@@ -31,4 +41,20 @@ void displayDigit(int num) {
   for (int i = 0; i < 7; i++) {
     digitalWrite(segmentPins[i], digits[num][i]);
   }
+}
+
+void startBeep() {
+  digitalWrite(buzzerPin, HIGH);  
+  delay(500);                      
+  digitalWrite(buzzerPin, LOW);  
+}
+
+void endBeep() {
+  digitalWrite(buzzerPin, HIGH);  
+  delay(300);                      
+  digitalWrite(buzzerPin, LOW);  
+  delay(400);                     
+  digitalWrite(buzzerPin, HIGH);  
+  delay(300);                    
+  digitalWrite(buzzerPin, LOW);   
 }
